6e81bc64f1c4a17d768113c30a7225e710b87ac8
Satoshi Watanabe <unksato@gmail.com>
2017-03-12 18:26:07 +0900

    add TypeScript code

---
+++ mario_world.ts
@@ -0,0 +1,251 @@
+namespace Charactor {
+    export class Mario {
+        private static STEP = 2;
+        private static COLOR_PALLET = ['','#000000','#ffffff','#520000','#8c5a18','#21318c','#ff4273','#b52963','#ffde73','#dea539','#ffd6c6','#ff736b','#84dece','#42849c'];
+        private static MARIO_CHAR = [[
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 8, 6, 3, 0, 0, 0],
+            [ 0, 0, 0, 0, 3, 6, 6, 7, 7, 9, 8, 2, 3, 0, 0, 0],
+            [ 0, 0, 0, 3, 7, 6, 7, 7, 1, 1, 1, 1, 1, 1, 0, 0],
+            [ 0, 0, 3, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [ 0, 0, 3,10, 1, 1, 1,11, 1,11, 1,11, 0, 0, 0, 0],
+            [ 0, 3,10, 4,10, 1,11,10, 1,10, 1,10, 4, 4, 0, 0],
+            [ 0, 3,11, 4,10, 1, 1,10,10,10,10,10,10,10, 4, 0],
+            [ 0, 3, 1,11,10, 1,10,10, 1,11,11,11,11,11, 4, 0],
+            [ 0, 0, 1, 1,11,11,10, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+            [ 0, 0, 0, 1, 4, 4,11,11,11, 1, 1, 1, 1, 0, 0, 0],
+            [ 0, 0, 0, 0, 3, 7, 4, 4, 4, 4, 5, 0, 0, 0, 0, 0],
+            [ 0, 0, 0, 3, 7, 7, 6,13,13,12,12, 5, 0, 0, 0, 0],
+            [ 0, 0, 0, 3, 4, 4, 4,13, 2, 2,12, 2, 5, 0, 0, 0],
+            [ 0, 0, 0, 4, 2, 2, 2, 4, 2, 2,12, 2, 5, 0, 0, 0],
+            [ 0, 0, 0, 4, 2, 2, 4,13,13,13,12,12, 5, 0, 0, 0],
+            [ 0, 0, 0, 4, 2, 2, 4,13,13, 5,13, 5, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 4, 4, 4, 4, 1, 4, 1, 0, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 1, 4, 4, 4, 8, 1, 8, 1, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+        ], [
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 8, 6, 3, 0, 0, 0],
+            [ 0, 0, 0, 0, 3, 6, 6, 7, 7, 9, 8, 2, 3, 0, 0, 0],
+            [ 0, 0, 0, 3, 7, 6, 7, 7, 1, 1, 1, 1, 1, 1, 0, 0],
+            [ 0, 0, 3, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [ 0, 0, 3,10, 1, 1, 1,11, 1,11, 1,11, 0, 0, 0, 0],
+            [ 0, 3,10, 4,10, 1,11,10, 1,10, 1,10, 4, 4, 0, 0],
+            [ 0, 3,11, 4,10, 1, 1,10,10,10,10,10,10,10, 4, 0],
+            [ 0, 3, 1,11,10, 1,10,10, 1,11,11,11,11,11, 4, 0],
+            [ 0, 0, 1, 1,11,11,10, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+            [ 0, 0, 0, 1, 4, 4,11,11,11, 1, 1, 1, 1, 0, 0, 0],
+            [ 0, 0, 0, 4, 7, 7, 7, 4, 4, 4, 5, 0, 0, 0, 0, 0],
+            [ 0, 0, 0, 4, 4, 4, 4,13,13,12,12, 5, 0, 0, 0, 0],
+            [ 0, 0, 1, 4, 2, 2, 2, 4, 2, 2,12, 2, 5, 1, 1, 0],
+            [ 0, 1, 3, 4, 2, 2, 4, 4, 2, 2,12, 2, 1, 8, 1, 1],
+            [ 0, 1, 3, 4, 2, 2, 4,13,13,13,12, 5, 1, 4, 1, 1],
+            [ 0, 1, 3, 1, 4, 4,13,13,13,13, 5, 1, 4, 1, 1, 0],
+            [ 0, 1, 3, 8, 1, 0, 5, 5, 5, 5, 0, 1, 4, 1, 1, 0],
+            [ 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ],[
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0], 
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 2, 4, 0], 
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 2, 2, 2, 4], 
+            [ 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 2, 2, 2, 4], 
+            [ 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 8, 6, 3, 2, 4, 0], 
+            [ 0, 0, 0, 0, 3, 6, 6, 7, 7, 9, 8, 2, 3, 4, 4, 0], 
+            [ 0, 0, 0, 3, 7, 6, 7, 7, 1, 1, 1, 1, 1, 1, 0, 0], 
+            [ 0, 0, 3, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+            [ 0, 0, 3,10, 1, 1, 1,11, 1,11, 1,11, 3, 0, 0, 0], 
+            [ 0, 3,10, 4,10, 1,11,10, 1,10, 1,10, 4, 4, 0, 0], 
+            [ 0, 3,11, 4,10, 1, 1,10,10,10,10,10,10,10, 4, 0], 
+            [ 0, 3, 1,11,10, 1,10,10, 1,11,11,11,11,11, 4, 0], 
+            [ 0, 0, 1, 1,11,11,10, 1, 1, 1, 1, 1, 1, 1, 0, 0], 
+            [ 0, 4, 4, 4, 7, 4,11,11,11, 1, 1, 1, 1, 0, 0, 0], 
+            [ 4, 4, 2, 2, 4, 7, 4, 4, 4, 4, 5, 0, 0, 0, 0, 0], 
+            [ 4, 2, 2, 2, 2, 4, 7,13,13,12,12, 5, 0, 0, 0, 0], 
+            [ 4, 2, 2, 2, 2, 4,13,13, 2, 2,12, 2, 5, 1, 1, 0], 
+            [ 0, 4, 2, 2, 4,13,13,13, 2, 2,12, 2, 1, 8, 1, 1], 
+            [ 0, 1, 3, 4,13,13,13,13,13,13,12, 5, 1, 4, 1, 1], 
+            [ 0, 1, 3, 5, 5, 5,13,13,13,13, 5, 1, 4, 1, 1, 0], 
+            [ 0, 1, 3, 8, 1, 0, 5, 5, 5, 5, 0, 1, 4, 1, 1, 0], 
+            [ 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0] 
+        ], [
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+            [ 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0], 
+            [ 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 8, 6, 3, 0, 0, 0], 
+            [ 0, 0, 0, 0, 3, 6, 6, 7, 7, 9, 8, 2, 3, 0, 0, 0], 
+            [ 0, 0, 0, 3, 7, 6, 7, 7, 1, 1, 1, 1, 1, 1, 0, 0], 
+            [ 0, 0, 3, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+            [ 0, 0, 3, 7, 1, 1, 1, 1, 1,11, 1, 1, 0, 0, 0, 0], 
+            [ 0, 3, 7, 7, 1, 1,10,10,10,10,10,10, 0, 0, 0, 0], 
+            [ 0, 3, 7,10, 1, 1,10,10, 1,10, 1,10, 0, 0, 0, 0], 
+            [ 0, 3,10, 4,10, 1,10,10, 1,10, 1,10, 4, 4, 0, 0], 
+            [ 0, 1,11, 4,10, 1, 1,10,10,10,10,10,10,10, 4, 0], 
+            [ 0, 0, 1,11,11, 1,10,10, 1,11,11,11,11,11, 4, 0], 
+            [ 0, 4, 2, 2, 3,11,11, 1, 1, 1, 1, 1, 1, 1, 2, 3], 
+            [ 4, 2, 2, 2, 2, 3,11,11,11, 1, 1, 1, 1, 2, 2, 3], 
+            [ 4, 2, 2, 2, 2, 3, 4, 4, 4, 4, 5, 4, 2, 1, 1, 0], 
+            [ 0, 4, 2, 2, 3,13,13,13,13,12,12, 5, 1, 8, 1, 1], 
+            [ 0, 1, 3, 3, 5,13,13,13, 2, 2,12, 2, 1, 4, 1, 1], 
+            [ 0, 1, 4, 4, 5,13,13,13, 2, 2,12, 2, 1, 4, 1, 1], 
+            [ 0, 1, 4, 8, 5, 5,13,13,13,13,12, 5, 1, 4, 1, 1], 
+            [ 0, 0, 1, 1, 0, 5, 5,13,13,13, 5, 0, 0, 1, 1, 0], 
+            [ 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0], 
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]            
+        ]];
+
+        private static DEFAULT_CSS_TXT = "z-index: 999; position: absolute; bottom: 0;";
+
+        private _actions : HTMLCanvasElement[] = [];
+        private _reverseActions : HTMLCanvasElement[]  = [];
+        private _actionIndex = 0;
+        private _prevActionIndex = 0;
+        private _currentAction : HTMLCanvasElement = null;
+        private _isReverse = false;
+        private _isJumping = false;
+        private _currentStep = Mario.STEP;
+        private _timer = null;
+        private _cssText = Mario.DEFAULT_CSS_TXT;
+        private _yVector = 0;
+        private _jumpPower = 12;
+        private _gravity = 2;
+        private positionY = 0;
+
+
+        constructor(private targetDom, private positionX: number = 0, private pixSize: number = 2) {
+            for (let charactor of Mario.MARIO_CHAR) {
+                this._actions.push(this.createCharactorAction(charactor));
+                this._reverseActions.push(this.createCharactorAction(charactor, true));
+            }
+        }
+
+        private createCharactorAction(charactorMap: number[][], isReverse:boolean = false) : HTMLCanvasElement{
+            let element = document.createElement("canvas");
+            let ctx = element.getContext("2d");
+            let charWidth = this.pixSize * 16;
+            let charHeight = this.pixSize * 22;
+            element.setAttribute("width", charWidth.toString());
+            element.setAttribute("height", charHeight.toString());
+            element.style.cssText = this._cssText;
+            Mario.drawCharactor(ctx, charactorMap, Mario.COLOR_PALLET, this.pixSize, isReverse);
+            return element;
+        }    
+
+        private static drawCharactor(ctx:CanvasRenderingContext2D, map:number[][], colors:string[], size:number, isReverse: boolean) : void {
+            for (let y = 0; y < map.length; y++){
+                if (isReverse)
+                    map[y].reverse();
+                for (let x = 0; x < map[y].length; x++){
+                    if (map[y][x] != 0) {
+                        ctx.beginPath();
+                        ctx.rect(x * size, y * size, size, size);
+                        ctx.fillStyle = colors[map[y][x]];
+                        ctx.fill();
+                    }
+                }
+                if (isReverse) {
+                    map[y].reverse();
+                }
+            }
+        }
+
+        private removeCharactor(): void {
+            if (this._currentAction != null) {
+                this.targetDom.removeChild(this._currentAction);
+                this._currentAction = null;
+            }
+        }
+
+        private drawAction(): void {
+            let target = this.targetDom;
+            this.removeCharactor();
+
+            if (this._currentStep < Mario.STEP) {
+                this._currentStep++;
+            } else {
+                this._currentStep = 0;
+                this._prevActionIndex = this._actionIndex;
+                this._actionIndex = this._actionIndex ^ 1;
+            }
+
+            this._currentAction = !this._isReverse ? this._actions[this._actionIndex] : this._reverseActions[this._actionIndex]; 
+
+
+            if (this._isJumping) {
+                this._yVector -= this._gravity;
+                this.positionY = this.positionY + this._yVector;
+
+                if (this.positionY <= 0) {
+                    this._isJumping = false;
+                    this._yVector = 0;
+                    this.positionY = 0;
+                } else {
+                    if (this._yVector > 0) {
+                        this._currentAction = !this._isReverse ? this._actions[2] : this._reverseActions[2];                         
+                    } else {
+                        this._currentAction = !this._isReverse ? this._actions[3] : this._reverseActions[3];                                                 
+                    }
+                }
+                this._currentAction.style.bottom = this.positionY + 'px';
+            }
+
+            this._currentAction.style.left = this.positionX + 'px';
+
+            this.targetDom.appendChild(this._currentAction);
+            if (this.positionX > this.targetDom.clientWidth - this.pixSize * 17 && this._isReverse == false) {
+                this._isReverse = true;
+            }
+            if (this.positionX < 0 && this._isReverse == true) {
+                this._isReverse = false;
+            }
+            if (!this._isReverse) {
+                this.positionX += this.pixSize * 2;
+            } else {
+                this.positionX -= this.pixSize * 2;            
+            }
+        }
+
+        public start(): void {
+            this.registerJump();
+            this._timer = setInterval(() => { this.drawAction() }, 35);
+        }
+
+        public stop(): void {
+            if (this._timer) {
+                clearInterval(this._timer);
+                this._timer = 0;
+            }
+        }
+
+        public registerJump(): void {
+            document.addEventListener("keypress", (e) => {
+                if (e.keyCode == 32) {
+                    this.jump();
+                }
+            });
+        }
+
+        private jump() {
+            if (!this._isJumping) {
+                this._isJumping = true;
+                this._yVector = this._jumpPower * this.pixSize;
+            }
+        }
+
+        public draw(index: number, isReverse: boolean = false, left: number = 0): void {
+            this._currentAction = !isReverse ? this._actions[index] : this._reverseActions[index]; 
+            this._currentAction.style.left = left*this.pixSize + 'px';
+            this.targetDom.appendChild(this._currentAction);
+        }
+
+        public remove(): void {
+            this.stop();
+            this.removeCharactor();
+        }
+    }
+}
+
+var mario = new Charactor.Mario(document.body)
+mario.start();
