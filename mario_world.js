27e85696788c1394e65c924eafef9c51544d10ff
Satoshi Watanabe <unksato@gmail.com>
2017-03-26 09:27:05 +0900

    update deviceorientation code

--- mario_world.js
+++ mario_world.js
@@ -621,8 +621,7 @@ var Character;
                 });
                 window.addEventListener('deviceorientation', function (e) {
                     var gamma = Math.round(e.gamma);
-                    document.body.innerText = gamma.toString();
-                    if (Math.abs(gamma) > 15 && _this._canSpeedUpForMobile) {
+                    if (Math.abs(gamma) >= 40 && _this._canSpeedUpForMobile) {
                         if (_this._isReverse && gamma > 0) {
                             _this._canSpeedUpForMobile = false;
                             _this.onSpeedUp();
