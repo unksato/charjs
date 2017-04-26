b986969821a6bda569f39c671e70635546812a5c
unksato <unksato@gmail.com>
2017-04-26 19:21:21 +0900

    create new mountain logic

--- src/character/object/mountain.ts
+++ src/character/object/mountain.ts
@@ -1,6 +1,14 @@
 namespace Charjs {
+    interface IMountData {
+        start: number;
+        pattern: number[];
+        color: string;
+        currentOffset?: number;
+    }
+
     export abstract class AbstractMountain extends AbstractPixel {
         abstract colors: string[];
+        abstract dataPattern: IMountData[];
         constructor(private width: number, private height: number, private pixSize: number, private type: number) {
             super();
         }
@@ -37,14 +45,43 @@ namespace Charjs {
             return element;
         }
 
+        draw2(): HTMLCanvasElement {
+            let element = AbstractMountain.createCanvasElement(this.width, this.height, 0);
+            let ctx = element.getContext("2d");
+            let center = this.width / this.pixSize / 2;
+
+
+            for (let i = 0; i < this.height; i++) {
+            }
+
+            return element;
+        }
+
         private picWithMirror(center: number, ctx: CanvasRenderingContext2D, x: number, y: number, color: string) {
             AbstractMountain.drawPixel(ctx, x, y, this.pixSize, color);
             let mirrorX = center + (center - x) - 1;
             AbstractMountain.drawPixel(ctx, mirrorX, y, this.pixSize, color);
         }
+        private deepCopy<T>(obj:T): T {
+            return JSON.parse(JSON.stringify(obj));
+        }
     }
 
     export class Mountain01 extends AbstractMountain {
         colors = ['#6daf91', '#5d9f81', '#4d8f71'];
+
+        dataPattern = [{
+            start: 0,
+            pattern: [2,2,2,1],
+            color:'#6daf91'
+        },{
+            start: 2,
+            pattern: [2,2,2,1],
+            color: '#5d9f81'
+        },{
+            start: 4,
+            pattern: [2,3,1],
+            color: '#4d8f71'
+        }];
     }
 }
