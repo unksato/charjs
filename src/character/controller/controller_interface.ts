7eabdb27b59bef10dbbfdd1456dd43982d94de05
unksato <unksato@gmail.com>
2017-05-23 16:05:55 +0900

    add controller interface

---
+++ src/character/controller/controller_interface.ts
@@ -0,0 +1,13 @@
+namespace Charjs {
+    export interface IButtonAssign{
+        right: number;
+        left: number;
+        up: number;
+        down: number;
+        a: number;
+        b: number;
+        x: number;
+        y: number;
+        pause: number;
+    }
+}
