ed31a35164ba6285b8bb096cabbb1045ec60ba02
Satoshi Watanabe <unksato@gmail.com>
2017-06-11 23:18:10 +0900

    WIP for webetc play

--- src/abstract_game_peer.ts
+++ src/abstract_game_peer.ts
@@ -1,12 +1,14 @@
 namespace Charjs {
     export abstract class AbstractGamePeer extends GameMaster {
         protected _peer: PeerConnector = null;
+        protected _peerId: string = null;
 
         public createPeer(): PeerConnector {
             if (!this._peer)
                 this._peer = PeerConnector.getPeer();
             return this._peer;
         }
+
 
     }
 }
